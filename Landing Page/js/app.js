624a1bb3756de5feb2358243e8cf95509f4af6c1
Rikard Linde <rikard@gmail.com>
2021-03-09 20:25:53 +0100

    Update app.js

--- Landing Page/js/app.js
+++ Landing Page/js/app.js
@@ -1,15 +1,59 @@
+// Insert sections in main
+
+function buildSection(quantity) {
+
+  let theMain = document.querySelector('main');
+
+  let sectionList = "";
+
+  let i = "1";
+
+  while (quantity > 0) {
+
+    if(i == "1") {var headline = "One";}
+    if(i == "2") {var headline = "Two";}
+    if(i == "3") {var headline = "Three";}
+    if(i == "4") {var headline = "Four";}
+    if(i == "5") {var headline = "Five";}
+    if(i == "6") {var headline = "Six";}
+    if(i == "7") {var headline = "Seven";}
+    if(i == "8") {var headline = "Eight";}
+
+    sectionList = "<section id='section" + i + "' data-nav='" + headline + "'><div class='landing__container'><h2>" + headline + "</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p><p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p></div></section>";
+
+    theMain.innerHTML += sectionList;
+
+    i++;
+
+    quantity--;
+
+  } 
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    buildSection('3');
+
+});
+
+
 // Navigation bar with links to sections
 
-const navBar = document.querySelector('#navbar__list');
-const sections = document.querySelectorAll("section");
+document.addEventListener("DOMContentLoaded", function() {
 
-sections.forEach(e => {
+    const navBar = document.querySelector('#navbar__list');
+    const sections = document.querySelectorAll("section");
 
-   const navList = "<li><a onclick='scrollToSection(" + e.id + ")'>" + e.dataset.nav + "</a></li>";
+    sections.forEach(e => {
 
-   navBar.innerHTML += navList;
+       const navList = "<li><a onclick='scrollToSection(" + e.id + ")'>" + e.dataset.nav + "</a></li>";
 
-})
+       navBar.innerHTML += navList;
+
+    })
+
+});
 
 
 // Hamburger icon displays or hides the navigation menu
